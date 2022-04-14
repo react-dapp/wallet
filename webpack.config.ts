d3e67a70e24dffbc0aa398a49c39565e7b42105a
Saqlain <saqlainprinters@gmail.com>
2022-04-14 09:00:30 +0500

    Updates for polyfills

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,7 @@ import * as path from "path";
 import webpackNodeExternals from "webpack-node-externals";
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
 import * as webpack from "webpack";
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
 
 const config: webpack.Configuration = {
   module: {
@@ -49,27 +50,8 @@ const config: webpack.Configuration = {
     modules: [path.resolve(__dirname, "/src"), "node_modules/"],
     descriptionFiles: ["package.json"],
     extensions: ["", ".js", ".ts", ".tsx", ".jsx"],
-    fallback: {
-      "os": false,
-      "fs": false,
-      "tls": false,
-      "url": false,
-      "net": false,
-      "util": false,
-      "path": false,
-      "zlib": false,
-      "http": false,
-      "https": false,
-      "stream": false,
-      "assert": false,
-      "crypto": false,
-      "crypto-browserify": false, //if you want to use this module also don't forget npm i crypto-browserify 
-      // "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
-    } 
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-  ],
+  plugins: [new CleanWebpackPlugin(), new NodePolyfillPlugin()],
 };
 
 export default config;
